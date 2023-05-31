cb4c2667e208ccbe33fc7bc7c4d12ef78ec9ab59
Logan Kulinski <rashes_lineage02@icloud.com>
2023-05-31 16:12:25 -0500

    ETA deserialization fix (#8)
    * ETA deserialization fix
    
    * Remove port specification
    
    * Deserialization fix

---
+++ src/main/java/app/cta4j/serialization/StringToIntegerConverter.java
@@ -0,0 +1,19 @@
+package app.cta4j.serialization;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import java.util.Objects;
+
+public final class StringToIntegerConverter extends StdConverter<String, Integer> {
+    @Override
+    public Integer convert(String string) {
+        Objects.requireNonNull(string);
+
+        String due = "DUE";
+
+        if (string.equalsIgnoreCase(due)) {
+            return 1;
+        }
+
+        return Integer.parseInt(string);
+    }
+}
