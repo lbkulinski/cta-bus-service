c77231c8d366f539aedbc4f83ee4ce996f78712c
Logan Kulinski <rashes_lineage02@icloud.com>
2023-07-20 12:50:07 -0500

    jOOQ caching (#17)
    * jOOQ caching updates
    
    * jOOQ caching updates
    
    * jOOQ caching updates

--- src/main/java/app/cta4j/client/BusClient.java
+++ src/main/java/app/cta4j/client/BusClient.java
@@ -1,24 +1,12 @@
 package app.cta4j.client;
 
 import app.cta4j.model.BusResponse;
-import app.cta4j.model.DirectionResponse;
-import app.cta4j.model.RouteResponse;
-import app.cta4j.model.StopResponse;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange
 public interface BusClient {
-    @GetExchange("/getroutes")
-    RouteResponse getRoutes();
-
-    @GetExchange("/getdirections")
-    DirectionResponse getRouteDirections(@RequestParam("rt") String id);
-
-    @GetExchange("/getstops")
-    StopResponse getRouteStops(@RequestParam("rt") String id, @RequestParam("dir") String direction);
-
     @GetExchange("/getpredictions")
     BusResponse getBuses(@RequestParam("rt") String routeId, @RequestParam("stpid") int stopId);
 }
--- src/main/java/app/cta4j/model/DirectionBody.java
+++
@@ -1,6 +0,0 @@
-package app.cta4j.model;
-
-import java.util.Set;
-
-public record DirectionBody(Set<Direction> directions) {
-}
