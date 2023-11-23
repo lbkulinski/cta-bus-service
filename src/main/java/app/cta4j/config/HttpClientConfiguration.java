2c1f10e0ff8aa98f20bf7db1d81b847034b461e8
Logan Kulinski <rashes_lineage02@icloud.com>
2023-11-23 10:52:02 -0600

    RestClient updates (#37)

--- src/main/java/app/cta4j/config/HttpClientConfiguration.java
+++ src/main/java/app/cta4j/config/HttpClientConfiguration.java
@@ -4,8 +4,8 @@ import app.cta4j.client.BusClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.Objects;
@@ -18,11 +18,11 @@ public class HttpClientConfiguration {
 
         String baseUrl = "https://ctabustracker.com/bustime/api/v2?key=%s&format=json".formatted(apiKey);
 
-        WebClient webClient = WebClient.create(baseUrl);
+        RestClient restClient = RestClient.create(baseUrl);
 
-        WebClientAdapter webClientAdapter = WebClientAdapter.forClient(webClient);
+        RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
 
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder(webClientAdapter)
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter)
                                                                                  .build();
 
         return httpServiceProxyFactory.createClient(BusClient.class);
